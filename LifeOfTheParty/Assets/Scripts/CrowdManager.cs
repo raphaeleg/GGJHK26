775d574149b074177ddcf15ddb56b1d4ebae0145
raphaeleg <raphaeleguillemotmichelle@gmail.com>
2026-01-31 06:01:39 +0800

    Base NPC algo brainstorming

---
+++ LifeOfTheParty/Assets/Scripts/CrowdManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdManager : MonoBehaviour
+{
+    // REF to CROWDREF, will MANIPULATE it
+    // USE black as base natural
+    // Object pool ALL NPCs (random) (200?) (place RANDOMLY, starting in black areas)
+    // Each has AI behaviour
+}
---
+++ LifeOfTheParty/Assets/Scripts/NPC.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    // STATE MACHINE
+    // Default behaviour: stick to black of CROWDREF, move slightly ish
+    // Attract behaviour
+    // Repel behaviour
+    // Freeze Behaviour, ignore black and stay still for x seconds (use GAMEMANAGER)
+}
