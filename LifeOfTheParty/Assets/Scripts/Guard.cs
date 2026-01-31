1f38453d05df5917adad1d56dd8711f802e3d178
Rie Tse <rietse68@gmail.com>
2026-01-31 16:26:59 +0800

    Guard Chase AI (starts chasing at Freeze Mask)

--- LifeOfTheParty/Assets/Scripts/Guard.cs
+++ LifeOfTheParty/Assets/Scripts/Guard.cs
@@ -4,12 +4,65 @@ using UnityEngine;
 
 public class Guard : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    [SerializeField]
+    private float speed;
 
-    public float speed = 5f;
+    [SerializeField]
+    private float rotationSpeed;
+
+    private Rigidbody2D rb;
+    public PlayerAwarenessController playerAwarenessController;
+    private Vector2 targetDirection;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerAwarenessController = GetComponent<PlayerAwarenessController>();
     }
+
+
+
+    private void FixedUpdate()
+    {
+        UpdateTargetDirection();
+        // RotateTowardsTarget();
+        SetVelocity();
+    } 
+
+    private void UpdateTargetDirection()
+    {
+        if (playerAwarenessController.awareOfPlayer)
+        {
+            targetDirection = playerAwarenessController.directionToPlayer;
+        }
+        else
+        {
+            targetDirection = Vector2.zero;
+        }
+    }
+
+    // private void RotateTowardsTarget()
+    // {
+    //     if (targetDirection == Vector2.zero)
+    //     {
+    //         return;
+    //     }
+    //     Quaternion targetRotation = Quaternion.LookRotation(transform.forward, targetDirection);
+    //     Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+    //     rb.SetRotation(rotation);
+    // }
+
+    private void SetVelocity()
+    {
+        if (targetDirection == Vector2.zero)
+        {
+            rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            rb.velocity = new Vector2(targetDirection.x, targetDirection.y) * speed;
+        }
+    }
+
 }
---
+++ LifeOfTheParty/Assets/Scripts/PlayerAwarenessController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAwarenessController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public bool awareOfPlayer;
+
+    public Vector2 directionToPlayer;
+
+    public Player player;
+
+    [SerializeField]
+    private float playerAwarenessDistance;
+
+    public float speed = 5f;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Update()
+    {
+        Vector2 enemyToPlayerVector = player.transform.position - this.transform.position;
+        directionToPlayer = enemyToPlayerVector.normalized;
+
+        if (enemyToPlayerVector.magnitude <= playerAwarenessDistance)
+        {
+            awareOfPlayer = true;
+        }
+        else
+        {
+            awareOfPlayer = false;
+        }
+    }
+}
