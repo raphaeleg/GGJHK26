e1ffa75fe7863eb8bf139851c95739c7387cd31e
Rie Tse <rietse68@gmail.com>
2026-01-31 16:52:25 +0800

    Readded Dash Mask
    because i was an idiot before with the git commits

--- LifeOfTheParty/Assets/Scripts/Player.cs
+++ LifeOfTheParty/Assets/Scripts/Player.cs
@@ -6,25 +6,84 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
 
-    public float speed = 10.0f;
+    public float speed = 5.0f;
 
     private Vector2 movement;
+    private Vector2 moveDirection;
+    private bool isDashUnlocked = false;
+    private TrailRenderer trail;
+
+    [Header("Dash")]
+    [SerializeField] public float dashSpeed = 10.0f;
+    [SerializeField] public float dashDuration = 1.0f;
+    [SerializeField] public float dashCooldown = 5.0f;
+    bool isDashing;
+
+    private void Awake()
+    {
+        trail = GetComponentInChildren<TrailRenderer>();
+        trail.enabled = false;
+    }
+
+    private void Start()
+    {
+
+    } 
 
     private void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(movement.x, movement.y);
+        
+        if (Input.GetKeyDown(KeyCode.Alpha1) && isDashUnlocked) {
+                StartCoroutine(DashMask());
+        }
     }
 
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.CompareTag("Guard"))
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        string collider = other.gameObject.tag;
+        switch (collider)
         {
+            case "Guard":
             Debug.Log("Caught!");
+            break;
+
+            case "DashMask":
+            isDashUnlocked = true;
+            Debug.Log("Unlocked Dash Mask!");
+
+            Destroy(other.gameObject);
+            break;
         }
+    }
+
+    private IEnumerator DashMask()
+    {
+        isDashing = true;
+        trail.enabled = true;
+        this.gameObject.layer = LayerMask.NameToLayer("IgnoreNPCs");
+        rb.velocity = new Vector2(moveDirection.x * dashSpeed, moveDirection.y * dashSpeed);
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+        trail.enabled = false;
+        this.gameObject.layer = LayerMask.NameToLayer("Player");
     }
 }
